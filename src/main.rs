47736a133cf2689df43e856d3881ec555f5e3b2a
Martin Seon <hyunmui@outlook.kr>
2021-12-24 22:45:51 +0900

    add square function

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,23 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    // fn square(size: u32) -> Rectangle {
+    //     Rectangle {
+    //         width: size,
+    //         height: size,
+    //     }
+    // }
+}
+
+// You can divide another impl
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -36,4 +53,7 @@ fn main() {
 
     println!("rect2 is contained to rect1? {}", rect1.can_hold(&rect2));
     println!("rect3 is contained to rect1? {}", rect1.can_hold(&rect3));
+
+    // square function
+    println!("{:?}", Rectangle::square(20));
 }
